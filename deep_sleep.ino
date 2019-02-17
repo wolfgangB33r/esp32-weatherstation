ce63285e497cddc2566a88431264a21288eb4bf9
Wolfgang Beer <wolfgang.beer@ruxit.com>
2019-02-17 10:34:20 +0100

    Update deep_sleep.ino

--- deep_sleep.ino
+++ deep_sleep.ino
@@ -1,24 +1,20 @@
-/*
- * ESP8266 Deep sleep mode example
- * Rui Santos 
- * Complete Project Details http://randomnerdtutorials.com
- */
- 
+// the setup function runs once when you press reset or power the board
 void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(2, OUTPUT);
   Serial.begin(115200);
-  Serial.setTimeout(2000);
-
-  // Wait for serial to initialize.
-  while(!Serial) { }
-  
-  // Deep sleep mode for 30 seconds, the ESP8266 wakes up by itself when GPIO 16 (D0 in NodeMCU board) is connected to the RESET pin
-  Serial.println("I'm awake, but I'm going into deep sleep mode for 30 seconds");
-  ESP.deepSleep(30e6); 
-  
-  // Deep sleep mode until RESET pin is connected to a LOW signal (for example pushbutton or magnetic reed switch)
-  //Serial.println("I'm awake, but I'm going into deep sleep mode until RESET pin is connected to a LOW signal");
-  //ESP.deepSleep(0); 
 }
 
+
+
+// the loop function runs over and over again forever
 void loop() {
+  Serial.println("Start");
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  delay(10000);  // wait for a second
+  // Deep sleep mode for 30 seconds, the ESP8266 wakes up by itself when GPIO 16 (D0 in NodeMCU board) is connected to the RESET pin
+  Serial.println("Going to sleep now");
+  ESP.deepSleep(30e6);
 }
